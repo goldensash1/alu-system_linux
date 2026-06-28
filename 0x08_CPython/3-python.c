4f515e5bc7b676a3cc78624499d1e8be58c54fc6
Golden Sash <101018402+goldensash01@users.noreply.github.com>
2026-06-28 20:33:04 +0200

    Fix 3-python.c: explicit long casts and unsigned char for hex printing
    Py_ssize_t is int on 32-bit Ubuntu 14.04; %ld with int causes a
    -Wformat -Werror compile failure. Cast all Py_ssize_t values to long
    before printing and switch %02hhx+char to %02x+(unsigned char) to
    avoid any sign/type-mismatch warnings under -pedantic -std=c99.

--- 0x08_CPython/3-python.c
+++ 0x08_CPython/3-python.c
@@ -13,7 +13,7 @@ void print_python_float(PyObject *p);
  */
 void print_python_bytes(PyObject *p)
 {
-	Py_ssize_t size, i, limit;
+	long size, i, limit;
 	PyBytesObject *bytes;
 
 	setbuf(stdout, NULL);
@@ -26,19 +26,16 @@ void print_python_bytes(PyObject *p)
 	}
 
 	bytes = (PyBytesObject *)p;
-	size = ((PyVarObject *)p)->ob_size;
+	size = (long)((PyVarObject *)p)->ob_size;
 
 	printf("  size: %ld\n", size);
 	printf("  trying string: %s\n", bytes->ob_sval);
 
-	if (size + 1 < 10)
-		limit = size + 1;
-	else
-		limit = 10;
+	limit = (size + 1 < 10) ? size + 1 : 10;
 
 	printf("  first %ld bytes:", limit);
 	for (i = 0; i < limit; i++)
-		printf(" %02hhx", bytes->ob_sval[i]);
+		printf(" %02x", (unsigned char)bytes->ob_sval[i]);
 	printf("\n");
 }
 
@@ -50,7 +47,6 @@ void print_python_bytes(PyObject *p)
  */
 void print_python_float(PyObject *p)
 {
-	double value;
 	char *str;
 
 	setbuf(stdout, NULL);
@@ -62,8 +58,8 @@ void print_python_float(PyObject *p)
 		return;
 	}
 
-	value = ((PyFloatObject *)p)->ob_fval;
-	str = PyOS_double_to_string(value, 'r', 0, Py_DTSF_ADD_DOT_0, NULL);
+	str = PyOS_double_to_string(((PyFloatObject *)p)->ob_fval,
+				    'r', 0, Py_DTSF_ADD_DOT_0, NULL);
 	printf("  value: %s\n", str);
 	PyMem_Free(str);
 }
@@ -76,7 +72,7 @@ void print_python_float(PyObject *p)
  */
 void print_python_list(PyObject *p)
 {
-	Py_ssize_t size, i;
+	long size, i;
 	PyListObject *list;
 	PyObject *item;
 
@@ -88,12 +84,12 @@ void print_python_list(PyObject *p)
 		return;
 	}
 
-	size = ((PyVarObject *)p)->ob_size;
+	size = (long)((PyVarObject *)p)->ob_size;
 	list = (PyListObject *)p;
 
 	printf("[*] Python list info\n");
 	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", list->allocated);
+	printf("[*] Allocated = %ld\n", (long)list->allocated);
 
 	for (i = 0; i < size; i++)
 	{
