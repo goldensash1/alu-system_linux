f44100de878e2aa7ecdbf89682e30e8d4a202a94
Golden Sash <101018402+goldensash01@users.noreply.github.com>
2026-07-14 14:10:31 +0200

    Fix Betty style: split connection logic out of main() in 2-client.c
    main() was at 41 lines, just over Betty's 40-line limit; extract the
    getaddrinfo/socket/connect sequence into connect_to_host().

--- sockets/2-client.c
+++ sockets/2-client.c
@@ -6,6 +6,48 @@
 #include <sys/socket.h>
 
 /**
+ * connect_to_host - resolves a host/port and connects a TCP socket to it
+ * @host: the host to connect to
+ * @port: the port to connect to
+ *
+ * Return: the connected socket's file descriptor
+ */
+static int connect_to_host(const char *host, const char *port)
+{
+	int sock_fd;
+	struct addrinfo hints, *res;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+
+	if (getaddrinfo(host, port, &hints, &res) != 0)
+	{
+		fprintf(stderr, "Could not resolve host: %s\n", host);
+		exit(EXIT_FAILURE);
+	}
+
+	sock_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+	if (sock_fd == -1)
+	{
+		perror("socket");
+		freeaddrinfo(res);
+		exit(EXIT_FAILURE);
+	}
+
+	if (connect(sock_fd, res->ai_addr, res->ai_addrlen) == -1)
+	{
+		perror("connect");
+		freeaddrinfo(res);
+		close(sock_fd);
+		exit(EXIT_FAILURE);
+	}
+
+	freeaddrinfo(res);
+	return (sock_fd);
+}
+
+/**
  * main - connects to a listening TCP server
  * @argc: argument count
  * @argv: argument vector, argv[1] is the host, argv[2] is the port
@@ -15,7 +57,6 @@
 int main(int argc, char *argv[])
 {
 	int sock_fd;
-	struct addrinfo hints, *res;
 
 	if (argc < 3)
 	{
@@ -23,34 +64,7 @@ int main(int argc, char *argv[])
 		return (EXIT_FAILURE);
 	}
 
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-
-	if (getaddrinfo(argv[1], argv[2], &hints, &res) != 0)
-	{
-		fprintf(stderr, "Could not resolve host: %s\n", argv[1]);
-		return (EXIT_FAILURE);
-	}
-
-	sock_fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-	if (sock_fd == -1)
-	{
-		perror("socket");
-		freeaddrinfo(res);
-		return (EXIT_FAILURE);
-	}
-
-	if (connect(sock_fd, res->ai_addr, res->ai_addrlen) == -1)
-	{
-		perror("connect");
-		freeaddrinfo(res);
-		close(sock_fd);
-		return (EXIT_FAILURE);
-	}
-
-	freeaddrinfo(res);
-
+	sock_fd = connect_to_host(argv[1], argv[2]);
 	printf("Connected to %s:%s\n", argv[1], argv[2]);
 
 	close(sock_fd);
